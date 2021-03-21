5b9f0ef35cde034d314d9f361eb7617add5ba1eb
Ivan Nelyubov <ivan.nelyubov@autodesk.com>
2021-03-21 13:15:11 +0100

    initial

---
+++ src/components/App/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+function App() {
+  return <div className="App">app</div>
+}
+
+export default App
---
+++ src/components/CommentList/CommentList.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+function CommentList() {
+  return <div>comment list</div>
+}
+
+export default CommentList
